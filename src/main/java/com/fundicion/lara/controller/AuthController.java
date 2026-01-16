c1b280aafe2028cdd6b01d978b679925f611be23
“robertoAguilar1993” <roberto.aguilar@meltsan.com>
2026-01-16 00:38:01 -0600

    Fix Docker ENTRYPOINT for Spring Boot jar

--- src/main/java/com/fundicion/lara/controller/AuthController.java
+++ src/main/java/com/fundicion/lara/controller/AuthController.java
@@ -11,10 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "AUTH")
 @RestController
@@ -53,7 +50,7 @@ public class AuthController {
         return ApiResponse.ok(authService.register(userRequest));
     }
 
-    @PostMapping("/status")
+    @GetMapping("/status")
     @Operation(
             operationId = "status",
             summary = "status",
